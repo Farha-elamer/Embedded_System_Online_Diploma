1cfe46322c87d0b795eefc7e9f0b511e9858600c
Farha-elamer <67878409+Farha-elamer@users.noreply.github.com>
2024-08-28 17:05:58 +0300

    Delete emmbedded_c/unit3 directory

--- emmbedded_c/unit3/lesson3/lab1_with make file/app.c
+++
@@ -1,7 +0,0 @@
-#include "uart.h"
-
-unsigned char String_Buffer[100] = "Learn-in-depth : <Farha>";
-
-void main(void){
-	Uart_Send_String(String_Buffer);	
-}
--- emmbedded_c/unit3/lesson3/lab1_with make file/uart.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UART_H_
-#define _UART_H_
-
-
-
-void Uart_Send_String(unsigned char *P_tx_String);
-
-
-#endif 
